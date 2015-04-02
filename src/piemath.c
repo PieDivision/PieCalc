dbb5ccbe5791ad699ff63f88ba73b539c1cac25e
Ondřej Budai <ondrej@budai.cz>
2015-04-02 15:27:10 +0200

    Added basic functions to math library

---
+++ src/piemath.c
@@ -0,0 +1,50 @@
+#include <math.h>
+
+#include "piemath.h"
+
+double piemathAdd(double a, double b){
+	return a + b;
+}
+double piemathSubstract(double a, double b){
+	return a - b;
+}
+double piemathMultiply(double a, double b){
+	return a * b;
+}
+double piemathDivide(double a, double b){
+	if(b == 0){
+		return NAN;
+	}
+
+	return a / b;
+}
+double piemathLn(double a){
+	if(a <= 0){
+		return NAN;
+	}
+	return log(a);
+}
+double pieMathLog(double a){
+	if(a <= 0){
+		return NAN;
+	}
+	return log10(a);
+}
+double piemathSqrt(double a){
+	if(a < 0){
+		return NAN;
+	}
+	return sqrt(a);
+}
+double piemathCbrt(double a){
+	return cbrt(a);
+}
+double piemathSquare(double a){
+	return pow(a, 2);
+}
+double piemathPower(double a, int power){
+	return pow(a, power);
+}
+double piemathExp(double a){
+	return exp(a);
+}
---
+++ src/piemath.h
@@ -0,0 +1,16 @@
+#ifndef PIEMATH_H
+#define PIEMATH_H
+
+double piemathAdd(double a, double b);
+double piemathSubstract(double a, double b);
+double piemathMultiply(double a, double b);
+double piemathDivide(double a, double b);
+double piemathLn(double a);
+double pieMathLog(double a);
+double piemathSqrt(double a);
+double piemathCbrt(double a);
+double piemathSquare(double a);
+double piemathPower(double a, int power);
+double piemathExp(double a);
+
+#endif
