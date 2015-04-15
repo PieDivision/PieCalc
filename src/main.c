84d1f7abc0ed9be27eb21b1e56130a5c223b1253
Ondřej Budai <ondrej@budai.cz>
2015-04-15 17:21:59 +0200

    Fix signal connecting by defining functions to export

--- src/main.c
+++ src/main.c
@@ -42,14 +42,14 @@ char *get_func(const char *text)
 }
 
 // Přeložení textu tlačítek na funkce
-void function_clicked(GtkButton *w, GtkEntry *entry)
+G_MODULE_EXPORT void function_clicked(GtkButton *w, GtkEntry *entry)
 {
 	strcpy(text, gtk_entry_get_text(entry));
 	strcat(text, get_func(gtk_button_get_label(w)));
 	gtk_entry_set_text(entry, text);
 }
 
-void show_about(GtkButton *w, Gui *gui)
+G_MODULE_EXPORT void show_about(GtkButton *w, Gui *gui)
 {
 	gtk_dialog_run(GTK_DIALOG(gui -> about));
 	gtk_widget_hide(gui -> about);
@@ -62,7 +62,7 @@ void show_about(GtkButton *w, Gui *gui)
  * @param w Button which has sent the signal
  * @param entry The display of the calculator
  */
-void numeric_button_clicked(GtkButton *w, GtkEntry *entry)
+G_MODULE_EXPORT void numeric_button_clicked(GtkButton *w, GtkEntry *entry)
 {
 	strcpy(text, gtk_entry_get_text(entry));
 	strcat(text, gtk_button_get_label(w));
@@ -70,12 +70,12 @@ void numeric_button_clicked(GtkButton *w, GtkEntry *entry)
 }
 
 // Dynamický výpis výsledku do GtkLabel
-void dynamic_result(GtkButton *w, GtkLabel *label)
+G_MODULE_EXPORT void dynamic_result(GtkButton *w, GtkLabel *label)
 {
 	gtk_label_set_text(label, dynamicRes);
 }
 
-void arith_button_clicked(GtkButton *w, GtkEntry *entry)
+G_MODULE_EXPORT void arith_button_clicked(GtkButton *w, GtkEntry *entry)
 {
 	sprintf(dynamicRes, "%.2f", pieSolver((char *)gtk_entry_get_text(entry)));
 	strcpy(text, gtk_entry_get_text(entry));
@@ -84,7 +84,7 @@ void arith_button_clicked(GtkButton *w, GtkEntry *entry)
 }
 
 // Smaže poslední znak na obrazovce
-void delete_one(GtkButton *w, GtkEntry *entry)
+G_MODULE_EXPORT void delete_one(GtkButton *w, GtkEntry *entry)
 {
 	strcpy(text, gtk_entry_get_text(entry));
 	text[strlen(text) - 1] = 0;
@@ -92,19 +92,19 @@ void delete_one(GtkButton *w, GtkEntry *entry)
 }
 
 // Vymazání textového okna
-void clear_clicked(GtkButton *w, GtkEntry *entry)
+G_MODULE_EXPORT void clear_clicked(GtkButton *w, GtkEntry *entry)
 {
 	gtk_entry_set_text(entry, "");
 }
 
 // Vymazání GtkLabel
-void clear_label(GtkButton *w, GtkLabel *label)
+G_MODULE_EXPORT void clear_label(GtkButton *w, GtkLabel *label)
 {
 	gtk_label_set_text(label, "");
 }
 
 // Odeslání rovnice a výpis výsledku
-void send_equation(GtkButton *w, GtkEntry *entry)
+G_MODULE_EXPORT void send_equation(GtkButton *w, GtkEntry *entry)
 {
 	setlocale(LC_NUMERIC, "en_US.UTF-8");
 	sprintf(text, "%.2f", pieSolver( (char *)gtk_entry_get_text(entry) ));
@@ -112,36 +112,55 @@ void send_equation(GtkButton *w, GtkEntry *entry)
 }
 
 // Výpis výsledku do GtkLabel
-void label_equation(GtkButton *w, GtkLabel *label)
+G_MODULE_EXPORT void label_equation(GtkButton *w, GtkLabel *label)
 {
 	gtk_label_set_text(label, text);
 }
 
 int main(int argc, char *argv[])
 {
+	int counter = 0;
+	fprintf(stderr, "%d\n", counter++);  //0
 	GtkBuilder *gtkBuilder;
 	Gui *gui;
 
 	gui = g_slice_new(Gui);
 
+	fprintf(stderr, "%d\n", counter++);  //1
 	gtk_init(&argc, &argv);
+	fprintf(stderr, "%d\n", counter++);  //2
 
 	gtkBuilder = gtk_builder_new();
-	gtk_builder_add_from_file(gtkBuilder, "pie_calc.glade", NULL);
+	fprintf(stderr, "%d\n", counter++);  //3
+	GError *err = NULL;
+	printf("%d\n",gtk_builder_add_from_file(gtkBuilder, "pie_calc.glade", &err));
+	//printf("%s", err->message);
+
+	fprintf(stderr, "%d\n", counter++); //4
 	
 	gui -> window = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "PieCalc"));
 	gui -> about = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "aboutDialog"));
 	gui -> entry = GTK_ENTRY(gtk_builder_get_object(gtkBuilder, "entry1"));
+	fprintf(stderr, "%d\n", counter++);//5
 
 	gui -> label = GTK_LABEL(gtk_builder_get_object(gtkBuilder, "label1"));
+	fprintf(stderr, "%d\n", counter++);//6
 
 	gtk_builder_connect_signals (gtkBuilder, gui);
+	fprintf(stderr, "%d\n", counter++);//7
 
 	g_object_unref(G_OBJECT(gtkBuilder));
+	fprintf(stderr, "%d\n", counter++);//8
+
+	fprintf(stderr, "%p\n", gui->window);
+	fprintf(stderr, "%p\n", gui->about);
+	fprintf(stderr, "%p\n", gui->entry);
 
 	g_signal_connect(gui -> window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+	fprintf(stderr, "%d\n", counter++);//9
 
 	gtk_widget_show(gui -> window);
+	fprintf(stderr, "%d\n", counter++);//10
 
 	gtk_main();
 
