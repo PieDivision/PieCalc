15aa67990c82cae74150869affdf633be2a1ac68
Benys94 <benys94@gmail.com>
2015-04-16 17:45:36 +0200

    Comments for doxygen added

--- src/main.c
+++ src/main.c
@@ -1,13 +1,23 @@
+/**
+ * @file main.c
+ * @brief Main source. GTK control.
+ * @author David Benes, Stepanka Barotova
+**/
+
 #include <gtk/gtk.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <locale.h>
 
+/// Our awesome lib
 #include "piesolve.h"
 
+/// Global variables for reading strings from text entry and storing of the result
 char text[1000] = "";
 char dynamicRes[1000] = "";
 
+/// Struct for GTK elements
 typedef struct{
 	GtkWidget *window;
 	GtkWidget *about;
@@ -16,11 +26,13 @@ typedef struct{
 	GtkLabel *label;
 } Gui;
 
+/// Struct for translating gtk button label
 typedef struct{
 	char *btn;
 	char *function;
 } Translate;
 
+/// Table of translations, first is pattern and second is replacement
 Translate table[] = {
 	{"RND", "rand()"},
 	{"√", "sqrt("},
@@ -32,6 +44,12 @@ Translate table[] = {
 	{"x!", "!"}
 };
 
+/**
+ * @brief Translate function button into shape which will be compatible with piemath.h
+ *
+ * @param[text] text of gtk button label which could be translated
+ * @return Rutine returns translated text or unchanged text if couldn't be translated
+**/
 char *get_func(char *text)
 {
 	for(unsigned int i = 0; i < sizeof(table) / sizeof(Translate); i++){
@@ -56,21 +74,30 @@ G_MODULE_EXPORT void numeric_button_clicked(GtkButton *w, GtkEntry *entry)
 }
 
 // Dynamický výpis do GtkLabel from editable entry
+/**
+ * @brief This rutine control everything what happens in text entry and hadle it
+ *
+ * @param[entry] Main editable display of piecalc
+ * @param[label] Secondary display of piecal which shows dynamic result or error in equation
+**/
 G_MODULE_EXPORT void editable_result(GtkEntry *entry, GtkLabel *label)
 {
-	memset(&dynamicRes[0], 0, sizeof(dynamicRes));
-	memset(&text[0], 0, sizeof(text));
+	memset(&dynamicRes[0], 0, sizeof(dynamicRes)); 
+	memset(&text[0], 0, sizeof(text)); // Clear arrays
 	
 	strcpy(dynamicRes, gtk_entry_get_text(entry));
-	if(strlen(dynamicRes) != 0){
+	if(strlen(dynamicRes) != 0){	// Proceed only if text entry isn't empty because of SIGSEGV
 		strncpy(text, dynamicRes, strlen(dynamicRes) - 1);
 		
+		// Read last character of equation
 		char c = dynamicRes[strlen(dynamicRes) - 1];
 	
+		// if last character of equation is arithmetic operatin make dynamic result
 		if(c == '+' || c == '-' || c == '*' || c == '/'){
 			sprintf(dynamicRes, "%.2f", pieSolver((char *)text));
 			gtk_label_set_text(label, dynamicRes);
 		}
+		// If last character is equal to equal print final solution
 		else if(c == '='){
 			sprintf(dynamicRes, "%.2f", pieSolver((char *)text));
 			gtk_label_set_text(label, dynamicRes);
@@ -78,19 +105,29 @@ G_MODULE_EXPORT void editable_result(GtkEntry *entry, GtkLabel *label)
 		}
 	}
 	else
-		gtk_label_set_text(label, "");
-}
+		gtk_label_set_text(label, ""); // If text entry is empty, clear label too
+} 
 
-// Smaže poslední znak na obrazovce
+/**
+ * @brief Delete last character of text entry (eqation)
+ *
+ * @param[w] Button which has sent a signal
+ * @param[entry] Display of the calculator
+**/
 G_MODULE_EXPORT void delete_one(GtkButton *w, GtkEntry *entry)
 {
-	(void)w;
+	(void)w; // This is necessary because of -Wall argument of gcc
 	strcpy(text, gtk_entry_get_text(entry));
 	text[strlen(text) - 1] = 0;
 	gtk_entry_set_text(entry, text);
 }
 
-// Vymazání textového okna
+/**
+ * @brief This rutine clears arrays and text entry
+ *
+ * @param[w] Button whis has sent a signal
+ * @param[entry] Display of the calculator which has to be cleared
+**/
 G_MODULE_EXPORT void clear_clicked(GtkButton *w, GtkEntry *entry)
 {
 	(void)w;
@@ -139,37 +176,37 @@ G_MODULE_EXPORT void quitHelp(GtkButton *w, GtkDialog *d)
 
 int main(int argc, char *argv[])
 {
-
 	GtkBuilder *gtkBuilder;
-	Gui *gui;
+	Gui *gui;	// Struct for GUI declaration
 
-	gui = g_slice_new(Gui);
+	gui = g_slice_new(Gui); // Allocate memory for Gui struct
 
-	gtk_init(&argc, &argv);
+	gtk_init(&argc, &argv); 
 
 	gtkBuilder = gtk_builder_new();
-	GError *err = NULL;
+	GError *err = NULL; // Variable for error handling
 	gtk_builder_add_from_file(gtkBuilder, "pie_calc.glade", &err);
 
-	
+	// get object from .glade file into GUI struct	
 	gui -> window = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "PieCalc"));
 	gui -> about = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "aboutDialog"));
 	gui -> help = GTK_WIDGET(gtk_builder_get_object(gtkBuilder, "helpDialog"));
-	// gui -> entry = GTK_ENTRY(gtk_builder_get_object(gtkBuilder, "entry1"));
-	// gui -> label = GTK_LABEL(gtk_builder_get_object(gtkBuilder, "label1"));
 
 	gtk_builder_connect_signals (gtkBuilder, gui);
 
 	g_object_unref(G_OBJECT(gtkBuilder));
 
+	// Signal for exiting GTK window
 	g_signal_connect(gui -> window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
+	// Display GTK window
 	gtk_widget_show(gui -> window);
 
 	// Set locale to English - we like decimal point more than decimal comma
 	setlocale(LC_NUMERIC, "en_US.UTF-8");
 
+	// Main loop of GTK
 	gtk_main();
 
-	return 0;
-}
+	return EXIT_SUCCESS;
+} // End of rutine main
