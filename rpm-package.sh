80fbd8d3671f64c664ce2a88de78e81ade159c17
Ondřej Budai <ondrej@budai.cz>
2015-04-27 16:45:44 +0200

    Final adjustments to rpm packaging

--- rpm-package.sh
+++ rpm-package.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-make pack
-mv piecalc-1.0.tar.gz ~/rpmbuild/SOURCES/
+git archive --format tar "--prefix=$FULLNAME/" HEAD | gzip > ~/rpmbuild/SOURCES/piecalc-1.0.tar.gz
+
 cp data/pie_calc.spec ~/rpmbuild/SPECS/
 cd ~/rpmbuild/SPECS/
 rpmbuild -ba pie_calc.spec
